12a5877825513814ca7d9b65f96dffe80aa65caf
KevinCirne1 <121322605+KevinCirne1@users.noreply.github.com>
2024-09-30 12:14:19 -0300

    Produto.js

--- PetAdopt/src/datasets/Produto.js
+++ PetAdopt/src/datasets/Produto.js
@@ -2,26 +2,29 @@ let produtos = [
   {
     id: '1',
     imagemUrl:
-      'https://static8.depositphotos.com/1146155/840/i/450/depositphotos_8405161-stock-photo-yellow-labrador-retriever.jpg',
-    titulo: 'Cookie',
-    descricao: 'Feito de cacau 100%.',
-    valor: '10.0',
+      'https://media.istockphoto.com/id/1496991182/pt/foto/cute-cavalier-king-charles-spaniel-having-a-bath-at-home.jpg?s=1024x1024&w=is&k=20&c=QVRhy7AuUVbPJhAfTRZ4IpL-Wu-3lXwdIuoF3EoJbqw=',
+    titulo: 'Área de banho',
+    descricao: 'Sempre limpinho.',
+    animaisparadoacao: 'Cachorro',
+    servicos:'Tosa'
   },
   {
     id: '2',
     imagemUrl:
-      'https://images.unsplash.com/photo-1624128082323-beb6b8b508db?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    titulo: 'Patel de forno',
-    descricao: 'Feito com frango da Bom Todo.',
-    valor: '5.0',
+      'https://media.istockphoto.com/id/1369497153/pt/foto/grooming-the-domastic-cat-in-pet-grooming-track.jpg?s=1024x1024&w=is&k=20&c=FHtpRJg8b5JbExeurh14qk-y36LtNzRdXhi9RoZBlyQ=',
+    titulo: 'Tosa',
+    descricao: 'Deixando os pelos do seu amiguinho ainda mais belos.',
+    animaisparadoacao: 'Gato',
+    servicos:'Banho'
   },
   {
     id: '3',
     imagemUrl:
-      'https://images.unsplash.com/photo-1525385133512-2f3bdd039054?q=80&w=2570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    titulo: 'Suco de Laranja',
-    descricao: 'Feita com laranjas de colhetas tardias.',
-    valor: '2.0',
+      'https://media.istockphoto.com/id/1294911173/pt/foto/two-white-little-rabbits-eat-feed-from-a-plate-on-a-white-background-food-for-domestic-and.jpg?s=1024x1024&w=is&k=20&c=XYxgXWtYSpbQYblD-7dhg_kV_jmCya9tRB0UfcLH_As=',
+    titulo: 'Área de secagem',
+    descricao: 'Tudo oque você precisa em um só lugar.',
+    animaisparadoacao: 'Coelho',
+    servicos:'Secagem'
   },
 ];
 
